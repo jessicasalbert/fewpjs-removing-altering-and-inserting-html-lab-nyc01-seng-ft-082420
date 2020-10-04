da6898a0498087019e4b547be9b9feeba887ad73
Jessica Salbert <jessicarsalbert@gmail.com>
2020-10-04 23:25:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+<<<<<<< HEAD
 // Write your code here!
+=======
+// Write your code here
+
+>>>>>>> e38ab36cf56c040ad54b1f55f1e1bf8072fe5b21
 const element = document.querySelector("main#main")
 element.remove()
 
